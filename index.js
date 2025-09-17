efc32d2429915b757f9dd07da877f991d9a656b3
danielkamweru <daniel.kamweru@student.moringaschool.com>
2025-09-17 12:09:40 +0300

    filter lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(drivers => drivers.toLowerCase() === name.toLowerCase());
+}
+function fuzzyMatch(drivers, query) {
+  return drivers.filter(driver => driver.startsWith(query));
+}
+function matchName(drivers, nameToMatch){
+return drivers.filter(drivers => drivers.name === nameToMatch)
+}
+
+
+
+
